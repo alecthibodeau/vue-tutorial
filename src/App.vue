9637527a17ac3b342d34534e54fa0a3d82754685
alec t <alec@alect.me>
2022-07-30 16:24:42 -0400

    Complete Step 2 - Declarative Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+// component logic
+const message = ref('Hello World!');
+
+const counter = reactive({
+  count: 0
+})
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <h1>{{ message.split('').reverse().join('') }}</h1>
+  <div>
+    <p>count is: {{ counter.count }}</p>
+  </div>
 </template>
