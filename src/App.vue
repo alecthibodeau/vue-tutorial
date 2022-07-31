478bdbf97165833f2d20c7f5843c42c3f23357fd
alec t <alec@alect.me>
2022-07-31 19:47:04 -0400

    Complete Step 14 - Slots

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,11 @@
 import { ref } from 'vue';
 import ChildComp from './components/ChildComp.vue';
 
-const childMessage = ref('No child message yet');
-const secondaryMessage = ref('Nothing to report');
+const message = ref('from parent');
 </script>
 
 <template>
-  <ChildComp @response="(message, addedMessage) => {
-      childMessage = message;
-      secondaryMessage = addedMessage;
-    }"
-  />
-  <p>{{ `${childMessage} ${secondaryMessage}` }}</p>
+  <ChildComp>
+    Message: {{ message }}
+  </ChildComp>
 </template>
--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-const emit = defineEmits(['response']);
-
-/**
- * The first argument to emit() is the event name.
- * Any additional arguments are passed on to the event listener.
- */
-emit('response', 'hello from child', 'all is well');
-</script>
-
 <template>
-  <h2>Child component</h2>
+  <slot>Fallback content</slot>
 </template>
