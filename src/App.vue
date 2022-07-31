819ebdd82eb183d51ac0643d35c286c459cbf3d9
alec t <alec@alect.me>
2022-07-31 19:10:48 -0400

    Complete Step 11 - Components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,8 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-
-const count = ref(0);
-const todoId = ref(1);
-const todoData = ref(null);
-
-watch(count, (newCount) => {
-  // yes, console.log() is a side effect
-  console.log(`new count is: ${newCount}`);
-});
-
-watch(todoId, fetchData);
-
-async function fetchData() {
-  todoData.value = null
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
-  )
-  todoData.value = await res.json()
-}
-
-fetchData();
+import ChildComp from './components/ChildComp.vue';
 </script>
 
 <template>
-  <button @click="count += 1">Count</button>
-  <p>Todo id: {{ todoId }}</p>
-  <button @click="todoId += 1">Fetch next todo</button>
-  <p v-if="!todoData">Loading...</p>
-  <pre v-else>{{ todoData }}</pre>
+  <!-- render child component -->
+  <ChildComp />
 </template>
